8334fd87d500df4b839af17941d51e36a7d647f5
danialmurtaza <daniyalmurtaza123456@gmail.com>
2024-09-05 21:28:58 +0500

    add loader

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,54 @@
 import React, { useRef } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import { FaBars, FaTimes } from "react-icons/fa";
 
-
-const Header = ({maincontainer = ''}) => {
-    const navRef = useRef();
+const Header = ({ maincontainer = "" }) => {
+  const navRef = useRef();
 
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive");
-
   };
 
-    return(
-        <div className={`${maincontainer} nav-container`}>
-        
-        <div className="container">
-
-          <div className="logo">
-            <img src="./images/ML logo.png" alt="logo" />
-          </div>
-
-          <nav ref={navRef}>
-            <a href="/#"><Link to="/home">Home</Link></a>
-            <a href="/#"><Link to="/trending">Trending</Link></a>
-            <a href="/#"><Link to="/popular">Popular</Link></a>
-            <a href="/#"><Link to="/toprated">Top Rated</Link></a>
-            <a href="/#"><Link to="/upcoming">Up Coming</Link></a>
-
-            <div className="nav-btn nav-close-btn" onClick={() => showNavbar()}>
-              <FaTimes />
-            </div>
-          </nav>
-
-          <div className="icons-div">
-
-            <button className="search-btn"> Sign Up</button>
-
-            <div className="nav-btn nav-open-btn" onClick={() => showNavbar()}>
-              <FaBars />
-            </div>
-          </div>
+  return (
+    <div className={`${maincontainer} nav-container`}>
+      <div className="container">
+        <div className="logo">
+          <img src="./images/ML logo.png" alt="logo" />
         </div>
 
+        <nav ref={navRef}>
+          <a href="/#">
+            <Link to="/home">Home</Link>
+          </a>
+          <a href="/#">
+            <Link to="/trending">Trending</Link>
+          </a>
+          <a href="/#">
+            <Link to="/popular">Popular</Link>
+          </a>
+          <a href="/#">
+            <Link to="/toprated">Top Rated</Link>
+          </a>
+          <a href="/#">
+            <Link to="/upcoming">Up Coming</Link>
+          </a>
+
+          <div className="nav-btn nav-close-btn" onClick={() => showNavbar()}>
+            <FaTimes />
+          </div>
+        </nav>
+
+        <div className="icons-div">
+          {/* <button className="search-btn"> Sign Up</button> */}
+
+          <div className="nav-btn nav-open-btn" onClick={() => showNavbar()}>
+            <FaBars />
+          </div>
+        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Header;
--- src/components/TopRatedPage.jsx
+++ src/components/TopRatedPage.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import Header from "./Header";
+import Loader from "./Loader";
 
 const TopRatedPage = () => {
   const [topRatedMovies, setTopRatedMovies] = useState([]);
-  const isLoading = !topRatedMovies || topRatedMovies.length === 0;
+  const [isLoading, setIsLoading] = useState(true);
+
+  // const isLoading = !topRatedMovies || topRatedMovies.length === 0;
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -14,8 +17,10 @@ const TopRatedPage = () => {
           "https://movie-land-backend.onrender.com/top_rated"
         );
         setTopRatedMovies(response.data);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching movies:", error);
+        setIsLoading(false);
       }
     };
     fetchMovies();
@@ -27,24 +32,30 @@ const TopRatedPage = () => {
 
       <div className="movie-container">
         <h1 className="toprated-main-heading">
-          {/* Top Rated */}
-          {isLoading ? "Loading !!!" : "Top Rated"}
+          Top Rated
+          {/* {isLoading ? "Loading !!!" : "Top Rated"} */}
         </h1>
-        <div className="movie">
-          {topRatedMovies.map((movie) => (
-            <div className="movie-details" key={movie.id}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={movie.title}
-              />
-              <p className="movie-title">{movie.title}</p>
-              <p className="movie-date-rating">
-                Released: {movie.release_date}
-              </p>
-              <p className="movie-date-rating">Rating: {movie.vote_average}</p>
-            </div>
-          ))}
-        </div>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="movie">
+            {topRatedMovies.map((movie) => (
+              <div className="movie-details" key={movie.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                <p className="movie-title">{movie.title}</p>
+                <p className="movie-date-rating">
+                  Released: {movie.release_date}
+                </p>
+                <p className="movie-date-rating">
+                  Rating: {movie.vote_average}
+                </p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
